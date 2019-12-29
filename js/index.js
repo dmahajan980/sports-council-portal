b1d3d887978aa25e25bd93fcee136c93074b6e80
neel2902 <nilkamalthakuria2902@gmail.com>
2019-12-29 22:05:29 +0530

    hover changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,1 @@
-var hover = document.querySelectorAll(".hover");
-for (var i = 0; i < hover.length; i++) {
-  if (hover[i].matches(":hover")) {
-    var down = hover[i].querySelector(".card-title").querySelector(".down");
-    down.style.transform = "rotate(180deg)";
-  }
-}
+
